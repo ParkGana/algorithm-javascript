70f0cdf8ab81a6198a96165cd4890f213e06374e
ParkGana <gnchoco97@gmail.com>
2024-10-16 10:22:01 +0900

    solve : 배열의 유사도

---
+++ programmers/level0/120903.js
@@ -0,0 +1,31 @@
+/**************************************************/
+
+/* 배열의 유사도
+
+/**************************************************/
+
+// 정답
+function solution(s1, s2) {
+    let arr = s1.filter((word) => s2.includes(word));
+
+    return arr.length;
+}
+
+/**************************************************/
+
+// 참고
+function solution(s1, s2) {
+    let count = 0;
+
+    for (let word1 of s1) {
+        for (let word2 of s2) {
+            if (word1 === word2) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+/**************************************************/
