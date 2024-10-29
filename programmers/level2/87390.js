77f9f901441a4626779b91de5ed12b6eee90cbbc
ParkGana <gnchoco97@gmail.com>
2024-10-29 09:44:34 +0900

    fail : n^2 배열 자르기 (시간 초과)

--- programmers/level2/87390.js
+++ programmers/level2/87390.js
@@ -10,6 +10,9 @@
 
 /* 런타임 에러 */
 function solution(n, left, right) {
+    // [ 예상 원인 ]
+    // n이 최대 n^7이므로 n * n 크기의 2차원 배열을 만들려고 하면 메모리적으로 과부화 발생
+
     let answer = [];
 
     for (let i = 1; i <= n; i++) {
@@ -19,6 +22,28 @@ function solution(n, left, right) {
     }
 
     return answer.slice(left, right + 1);
+}
+
+/**************************************************/
+
+/* 시간 초과 */
+function solution(n, left, right) {
+    let answer = [];
+
+    for (let row = 1; row <= n; row++) {
+        for (let col = 1; col <= n; col++) {
+            const index = (row - 1) * n + col - 1;
+
+            if (index >= left && index <= right) {
+                answer.push(row > col ? row : col);
+                continue;
+            }
+
+            if (index > right) break;
+        }
+    }
+
+    return answer;
 }
 
 /**************************************************/
