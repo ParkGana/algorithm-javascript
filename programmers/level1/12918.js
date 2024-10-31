ae26122442179d7978ac689efbec45c085aa335f
ParkGana <gnchoco97@gmail.com>
2024-10-31 10:04:25 +0900

    fail : 문자열 다루기 기본 (실패)

--- programmers/level1/12918.js
+++ programmers/level1/12918.js
@@ -10,7 +10,21 @@
 
 /* 실패 */
 function solution(s) {
+    // [ 예상 원인 ]
+    // s가 0으로만 이루어진 문자열인 경우 숫자로 변환해도 falsy한 값으로 인식
+
     if (s.length !== 4 && s.length !== 6) return false;
+
+    return !!Number(s);
+}
+
+/**************************************************/
+
+/* 실패 */
+function solution(s) {
+    if (s.length !== 4 && s.length !== 6) return false;
+
+    if (Number(s) === 0) return true;
 
     return !!Number(s);
 }
